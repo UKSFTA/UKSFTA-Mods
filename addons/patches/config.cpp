a703865093d320df84e9bc256ef55873d7009618
MBarkerUK <matthewbarker@librem.one>
2025-04-11 23:59:48 +0100

    updated: config.cpp for new patches

---
+++ addons/patches/config.cpp
@@ -0,0 +1,258 @@
+class CfgPatches {
+    class UKSFTA_Insignia {
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = {};
+        version = "1.7.7";
+        author[] = {"Jack"};
+        authorUrl = "";
+    };
+};
+class CfgUnitInsignia {
+    class ACPL {
+        displayName = "Army - Cpl.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\jncos\army-cpl_patch_co.paa";
+        textureVehicle = "";
+    };
+    class ALCPL {
+        displayName = "Army - LCpl.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\jncos\army-lcpl_patch_co.paa";
+        textureVehicle = "";
+    };
+    class A2LT {
+        displayName = "Army - 2LT.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\off\army-2lt_patch_co.paa";
+        textureVehicle = "";
+    };
+    class ACAPT {
+        displayName = "Army - Capt.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\off\army-capt_patch_co.paa";
+        textureVehicle = "";
+    };
+    class ALT {
+        displayName = "Army - Lt.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\off\army-lt_patch_co.paa";
+        textureVehicle = "";
+    };
+    class ALTCOL {
+        displayName = "Army - Lt. Col.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\off\army-lt_col_patch_co.paa";
+        textureVehicle = "";
+    };
+    class AMAJ {
+        displayName = "Army - Maj.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\off\army-maj_patch_co.paa";
+        textureVehicle = "";
+    };
+    class APTE {
+        displayName = "Army - Pte.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\ors\army-pte_patch_co.paa";
+        textureVehicle = "";
+    };
+    class ATPR {
+        displayName = "Army - Tpr.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\ors\army-tpr_patch_co.paa";
+        textureVehicle = "";
+    };
+    class ASGT {
+        displayName = "Army - Sgt.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\sncos\army-sgt_patch_co.paa";
+        textureVehicle = "";
+    };
+    class ASSGT {
+        displayName = "Army - Ssgt.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\sncos\army-ssgt_patch_co.paa";
+        textureVehicle = "";
+    };
+    class ARQMS {
+        displayName = "Army - Rqms.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\wos\army-rqms_patch_co.paa";
+        textureVehicle = "";
+    };
+    class AWO1 {
+        displayName = "Army - Wo1.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\wos\army-wo1_patch_co.paa";
+        textureVehicle = "";
+    };
+    class AWO2 {
+        displayName = "Army - Wo2.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\army\wos\army-wo2_patch_co.paa";
+        textureVehicle = "";
+    };
+        class NLH {
+        displayName = "Navy - Lh.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\navy\jncos\navy-lh_patch_co.paa";
+        textureVehicle = "";
+    };
+    class NLT {
+        displayName = "Navy - Lt.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\navy\jncos\navy-lt_patch_co.paa";
+        textureVehicle = "";
+    };
+    class NLTCDR {
+        displayName = "Navy - Lt. Cdr.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\navy\off\navy-ltcdr_patch_co.paa";
+        textureVehicle = "";
+    };
+    class NSLT {
+        displayName = "Navy - Slt.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\navy\off\navy-slt_patch_co.paa";
+        textureVehicle = "";
+    };
+    class NAB {
+        displayName = "Navy - Ab.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\navy\ors\navy-ab_patch_co.paa";
+        textureVehicle = "";
+    };
+    class NCPO {
+        displayName = "Navy - Cpo.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\navy\sncos\navy-cpo_patch_co.paa";
+        textureVehicle = "";
+    };
+    class NPO {
+        displayName = "Navy - Cpo.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\navy\sncos\navy-po_patch_co.paa";
+        textureVehicle = "";
+    };
+    class NWO1 {
+        displayName = "Navy - Wo1.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\navy\wos\navy-wo1_patch_co.paa";
+        textureVehicle = "";
+    };
+    class NWO2 {
+        displayName = "Navy - Wo2.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\navy\wos\navy-wo2_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RAS1 {
+        displayName = "RAF - As1.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\jncos\raf-as1_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RAS1T {
+        displayName = "RAF - As1T.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\jncos\raf-as1t_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RCPL {
+        displayName = "RAF - Cpl.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\jncos\raf-cpl_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RLCPL {
+        displayName = "RAF - Lcpl.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\jncos\raf-lcpl_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RFLOFF {
+        displayName = "RAF - Fl. Off.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\off\raf-floff_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RFLTLT {
+        displayName = "RAF - Fl. Tlt.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\off\raf-fltlt_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RPLTOFF {
+        displayName = "RAF - Plt. Off.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\off\raf-pltoff_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RSQNLDR {
+        displayName = "RAF - Sqn. Ldr.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\off\raf-sqnldr_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RAM {
+        displayName = "RAF - AM.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\ors\raf-am_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RAS2 {
+        displayName = "RAF - AS2.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\ors\raf-as2_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RCTECH {
+        displayName = "RAF - Ct.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\sncos\raf-ctech_patch_co.paa";
+        textureVehicle = "";
+    };
+        class RFSACR {
+        displayName = "RAF - Fs. Acr.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\sncos\raf-fsacr_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RFSGT {
+        displayName = "RAF - F. Sgt.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\sncos\raf-fsgt_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RSACR {
+        displayName = "RAF - Sgt. Acr.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\sncos\raf-sacr_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RSGT {
+        displayName = "RAF - Sgt.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\sncos\raf-sgt_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RMACR {
+        displayName = "RAF - Mag. Cr.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\wos\raf-magcr_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RWO {
+        displayName = "RAF - Wo.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\wos\raf-wo_patch_co.paa";
+        textureVehicle = "";
+    };
+    class RWOAF {
+        displayName = "RAF - Wo. RAF.";
+        author = "Maj. S. Woods.";
+        texture = "z\uksfta\addons\patches\Textures\raf\wos\raf-woaf_patch_co.paa";
+        textureVehicle = "";
+    };
+};
