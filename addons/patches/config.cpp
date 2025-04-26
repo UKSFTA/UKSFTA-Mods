c316d1fa08bd3f216871596f17f879592fd4f5fa
MBarkerUK <matthewbarker@librem.one>
2025-04-26 19:05:47 +0100

    clean: cleaned up some potential issues

--- addons/patches/config.cpp
+++ addons/patches/config.cpp
@@ -11,247 +11,247 @@ class CfgPatches {
 class CfgUnitInsignia {
     class ACPL {
         displayName = "Army - Cpl.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\army\jncos\army-cpl_patch_co.paa";
         textureVehicle = "";
     };
     class ALCPL {
         displayName = "Army - LCpl.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\army\jncos\army-lcpl_patch_co.paa";
         textureVehicle = "";
     };
     class A2LT {
         displayName = "Army - 2Lt.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\army\off\army-2lt_patch_co.paa";
         textureVehicle = "";
     };
     class ACAPT {
         displayName = "Army - Capt.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\army\off\army-capt_patch_co.paa";
         textureVehicle = "";
     };
     class ALT {
         displayName = "Army - Lt.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\army\off\army-lt_patch_co.paa";
         textureVehicle = "";
     };
     class ALTCOL {
         displayName = "Army - Lt. Col.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\army\off\army-ltcol_patch_co.paa";
         textureVehicle = "";
     };
     class AMAJ {
         displayName = "Army - Maj.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\army\off\army-maj_patch_co.paa";
         textureVehicle = "";
     };
     class APTE {
         displayName = "Army - Pte.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\army\ors\army-pte_patch_co.paa";
         textureVehicle = "";
     };
     class ATPR {
         displayName = "Army[SF ONLY] - Tpr.";
-        author = "REDCoat Media.";
+        author = "S. Woods.";
         texture = "z\uksfta\addons\patches\Textures\army\ors\army-tpr_patch_co.paa";
         textureVehicle = "";
     };
     class ASGT {
         displayName = "Army - Sgt.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\army\sncos\army-sgt_patch_co.paa";
         textureVehicle = "";
     };
     class ASSGT {
         displayName = "Army - CSgt./SSgt.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\army\sncos\army-ssgt_patch_co.paa";
         textureVehicle = "";
     };
     class ARQMS {
         displayName = "Army - RQMS[WO2].";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\army\wos\army-rqms_patch_co.paa";
         textureVehicle = "";
     };
     class AWO1 {
         displayName = "Army - RSM[WO1].";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\army\wos\army-wo1_patch_co.paa";
         textureVehicle = "";
     };
     class AWO2 {
         displayName = "Army - CSM[WO2].";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\army\wos\army-wo2_patch_co.paa";
         textureVehicle = "";
     };
         class NLH {
         displayName = "Navy - LH.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\navy\jncos\navy-lh_patch_co.paa";
         textureVehicle = "";
     };
     class NLT {
         displayName = "Navy - Lt.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\navy\off\navy-lt_patch_co.paa";
         textureVehicle = "";
     };
     class NLTCDR {
         displayName = "Navy - Lt. Cdr.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\navy\off\navy-ltcdr_patch_co.paa";
         textureVehicle = "";
     };
     class NSLT {
         displayName = "Navy - Slt.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\navy\off\navy-slt_patch_co.paa";
         textureVehicle = "";
     };
     class NAB {
         displayName = "Navy - AB.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\navy\ors\navy-ab_patch_co.paa";
         textureVehicle = "";
     };
     class NCPO {
         displayName = "Navy - CPO.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\navy\sncos\navy-cpo_patch_co.paa";
         textureVehicle = "";
     };
     class NPO {
         displayName = "Navy - PO.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\navy\sncos\navy-po_patch_co.paa";
         textureVehicle = "";
     };
     class NWO1 {
         displayName = "Navy - WO1.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\navy\wos\navy-wo1_patch_co.paa";
         textureVehicle = "";
     };
     class NWO2 {
         displayName = "Navy - WO2.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\navy\wos\navy-wo2_patch_co.paa";
         textureVehicle = "";
     };
     class RAS1 {
         displayName = "RAF - AS1.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\jncos\raf-as1_patch_co.paa";
         textureVehicle = "";
     };
     class RAS1T {
         displayName = "RAF - AS1T.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\jncos\raf-as1t_patch_co.paa";
         textureVehicle = "";
     };
     class RCPL {
         displayName = "RAF - Cpl.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\jncos\raf-cpl_patch_co.paa";
         textureVehicle = "";
     };
     class RLCPL {
         displayName = "RAF - Lcpl.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\jncos\raf-lcpl_patch_co.paa";
         textureVehicle = "";
     };
     class RFLOFF {
         displayName = "RAF - Fl. Off.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\off\raf-floff_patch_co.paa";
         textureVehicle = "";
     };
     class RFLTLT {
         displayName = "RAF - Fl. Tlt.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\off\raf-fltlt_patch_co.paa";
         textureVehicle = "";
     };
     class RPLTOFF {
         displayName = "RAF - Plt. Off.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\off\raf-pltoff_patch_co.paa";
         textureVehicle = "";
     };
     class RSQNLDR {
         displayName = "RAF - Sqn. Ldr.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\off\raf-sqnldr_patch_co.paa";
         textureVehicle = "";
     };
     class RAM {
         displayName = "RAF - AM.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\ors\raf-am_patch_co.paa";
         textureVehicle = "";
     };
     class RAS2 {
         displayName = "RAF - AS2.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\ors\raf-as2_patch_co.paa";
         textureVehicle = "";
     };
     class RCTECH {
         displayName = "RAF - Chf Tech.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\sncos\raf-ctech_patch_co.paa";
         textureVehicle = "";
     };
         class RFSACR {
         displayName = "RAF - FSAcr.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\sncos\raf-fsacr_patch_co.paa";
         textureVehicle = "";
     };
     class RFSGT {
         displayName = "RAF - FSgt.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\sncos\raf-fsgt_patch_co.paa";
         textureVehicle = "";
     };
     class RSACR {
         displayName = "RAF - SAcr.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\sncos\raf-sacr_patch_co.paa";
         textureVehicle = "";
     };
     class RSGT {
         displayName = "RAF - Sgt.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\sncos\raf-sgt_patch_co.paa";
         textureVehicle = "";
     };
     class RMACR {
         displayName = "RAF - MACr.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\wos\raf-macr_patch_co.paa";
         textureVehicle = "";
     };
     class RWO {
         displayName = "RAF - WO.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\wos\raf-wo_patch_co.paa";
         textureVehicle = "";
     };
     class RWOAF {
         displayName = "RAF - WORAF.";
-        author = "REDCoat Media";
+        author = "S. Woods";
         texture = "z\uksfta\addons\patches\Textures\raf\wos\raf-woaf_patch_co.paa";
         textureVehicle = "";
     };
