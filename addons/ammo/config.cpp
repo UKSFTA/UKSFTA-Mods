21bbb6d479d350875e1ad8914a6b52b1cbb78219
MBarkerUK <matthewbarker@librem.one>
2025-05-09 21:49:30 +0100

    added: L119A1 to Ammo Compat

--- addons/ammo/config.cpp
+++ addons/ammo/config.cpp
@@ -108,4 +108,30 @@ class CfgWeapons {
             "SF_30Rnd_762x35_SS"
         };
     };
+    class SF_L119A1_CQB: SF_L119_BASE {
+        displayName = "[UKSF] C8 SFW";
+        magazines[] = {
+            "rhs_mag_30Rnd_556x45_M855_Stanag",
+            "rhs_mag_30Rnd_556x45_M855_Stanag_Tracer_Red",
+            "rhs_mag_30Rnd_556x45_M855_Stanag_Tracer_Green",
+            "rhs_mag_30Rnd_556x45_M855_Stanag_Tracer_Yellow",
+            "rhs_mag_30Rnd_556x45_M855_Stanag_Tracer_Orange",
+            "rhs_mag_30Rnd_556x45_M855A1_Stanag",
+            "rhs_mag_30Rnd_556x45_M855A1_Stanag_No_Tracer",
+            "rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red",
+            "rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Green",
+            "rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Yellow",
+            "rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Orange",
+            "rhs_mag_30Rnd_556x45_Mk318_Stanag",
+            "rhs_mag_30Rnd_556x45_Mk262_Stanag",
+            "rhs_mag_30Rnd_556x45_M200_Stanag",
+            "30Rnd_556x45_Stanag",
+            "30Rnd_556x45_Stanag_Tracer_Red",
+            "30Rnd_556x45_Stanag_Tracer_Green",
+            "30Rnd_556x45_Stanag_Tracer_Yellow",
+            "UK3CB_BAF_556_30Rnd",
+            "UK3CB_BAF_556_30Rnd_Blank",
+            "UK3CB_BAF_556_30Rnd_T"
+        };
+    };
 };
