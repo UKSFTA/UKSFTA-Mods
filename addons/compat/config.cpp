b0d832ccd1c0c46fd285762f7c4db68d1cf669b3
Matthew Barker <matthewbarker@librem.one>
2026-02-11 15:38:32 +0000

    fix: consolidate LHA boat patch and realistic vehicle durability in addons/compat

--- addons/compat/config.cpp
+++ addons/compat/config.cpp
@@ -5,12 +5,13 @@
 class CfgPatches {
     class UKSFTA_Mods_Compat {
         name = "UKSF Taskforce Alpha Mods - Compatibility & Fixes";
-        units[] = {};
+        units[] = {"UKSF_LHA"};
         weapons[] = {};
         requiredVersion = 1.62;
         requiredAddons[] = {
             "UKSFTA_Mods_Main",
-            "Arlit_Toyota"
+            "Arlit_Toyota",
+            "Peral_LHA"
         };
         author = "UKSF Taskforce Alpha Team";
         authors[] = {"UKSF Taskforce Alpha Team"};
@@ -22,12 +23,24 @@ class CfgPatches {
 
 class CfgVehicles {
     class Car_F;
+    class Peral_LHA;
 
-    // 1. Toyota Pickup Base - Reinforced (Ignores 5.56mm)
+    // --- LHA Boat Patch ---
+    class UKSF_LHA: Peral_LHA {
+        scope = 2; 
+        displayName = "UKSF LHA";
+        editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\Default\default_editorPreview.paa";
+        Aircraft[] = {"fza_ah64d_b2e","TF373_RAF_HC5","vtx_MH60M","UK3CB_BAF_Wildcat_AH1_TRN_8A_Tropical"};
+        Amphibious[] = { "", "", "" };
+        Boats[] = {{"rhsgref_hidf_assault_boat",8,1,0.2},{"UK3CB_BAF_RHIB_HMG_MTP_RM",9,1.2,2},{"UK3CB_BAF_RHIB_GPMG_MTP_RM",9,1.2,2}};
+        hiddenSelections[] = {"screen_01","screen_02","lha_number","flag_01","flag_02"};
+        hiddenSelectionsTextures[] = {"Peral_LHA\data\clear_empty_ca.paa","Peral_LHA\data\clear_empty_ca.paa","Peral_LHA\data\clear_empty_ca.paa","\A3\Data_F\Flags\flag_uk_co.paa","Peral_LHA\data\clear_empty_ca.paa"};
+    };
+
+    // --- Toyota Pickup Base - Reinforced (Ignores 5.56mm) ---
     class Arlit_TOYOTA_BASE: Car_F {
         armor = 300;
         armorStructural = 8;
-        
         wheelDamageThreshold = 0.7;
         wheelDestroyThreshold = 0.9;
 
@@ -47,16 +60,15 @@ class CfgVehicles {
             class HitRF2Wheel: HitLFWheel {};
 
             class HitBody { armor = 30; minimalHit = 0.1; passThrough = 0.1; };
-            class HitEngine { armor = 40; minimalHit = 0.15; passThrough = 0.05; }; // Stops 5.56
+            class HitEngine { armor = 40; minimalHit = 0.15; passThrough = 0.05; };
             class HitFuel { armor = 40; minimalHit = 0.15; passThrough = 0.05; };
         };
     };
 
-    // 2. Land Cruiser 200 Base - Armoured Spec (Ignores 5.56mm & 7.62mm)
+    // --- Land Cruiser 200 Base - Armoured Spec (Ignores 5.56mm & 7.62mm) ---
     class arlit_200_16_base: Car_F {
         armor = 550;
         armorStructural = 10;
-
         wheelDamageThreshold = 0.8;
         wheelDestroyThreshold = 1.0;
 
@@ -74,7 +86,7 @@ class CfgVehicles {
             class HitRF2Wheel: HitLFWheel {};
 
             class HitBody { armor = 50; minimalHit = 0.2; passThrough = 0.1; };
-            class HitEngine { armor = 60; minimalHit = 0.35; passThrough = 0.05; }; // Stops 5.56 and 7.62
+            class HitEngine { armor = 60; minimalHit = 0.35; passThrough = 0.05; };
             class HitFuel { armor = 60; minimalHit = 0.35; passThrough = 0.05; };
         };
     };
