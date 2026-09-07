426fedd541fc57dcd1befd16792371037d8ff71c
Matthew Barker <matthewbarker@librem.one>
2026-09-07 15:10:29 +0100

    compat: remove DIS incognito integration layer
    Removes the Dynamic Incognito System custom compatibility code:
    XEH_postInit patch, mainLoop function, and ACE Arsenal config.
    Drops DIS_incog and Peral_LHA from compat CfgPatches
    requiredAddons and cleans the empty function list.

--- addons/compat/config.cpp
+++ addons/compat/config.cpp
@@ -5,58 +5,20 @@
 class CfgPatches {
     class UKSFTA_Mods_Compat {
         name = "UKSF Taskforce Alpha Mods - Compatibility & Fixes";
-        units[] = {"UKSF_LHA"}; // Hook will sync these
+        units[] = {}; // Hook will sync these
         weapons[] = {};
         requiredVersion = 1.64;
         requiredAddons[] = {
             "UKSFTA_Mods_Main", // Load main first to establish categories
             "A3_Weapons_F",
-            "Peral_LHA",
             "rhsusf_c_weapons",
-            "Louetta_seven",
-            "DIS_incog"
+            "Louetta_seven"
         };
         author = "UKSF Taskforce Alpha Team";
         authors[] = {"UKSF Taskforce Alpha Team"};
         version = QUOTE(MAJOR.MINOR.PATCHLVL);
         versionStr = QUOTE(MAJOR.MINOR.PATCHLVL);
         versionAr[] = {MAJOR,MINOR,PATCHLVL};
-    };
-};
-
-class Extended_PostInit_EventHandlers {
-    class DIS_incog_Patch {
-        init = "call compile preprocessFileLineNumbers 'z\uksfta\addons\compat\XEH_postInit.sqf'";
-    };
-};
-
-
-class CfgFunctions {
-    class DIS {
-        class incog {
-            file = "z\uksfta\addons\compat\functions";
-            class mainLoop {};
-        };
-    };
-};
-
-class CfgVehicles {
-    class Peral_LHA;
-    // --- UKSF LHA (Reinforced Variant) ---
-    class UKSF_LHA: Peral_LHA {
-        scope = 2;
-        scopeCurator = 2;
-        side = 1;
-        faction = "UKSFTA_Faction";
-        editorCategory = "UKSFTA_Editor_Cat";
-        editorSubcategory = "UKSFTA_SubCat_Vehicles";
-        displayName = "[UKSF] LHA";
-        editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\Default\default_editorPreview.paa";
-        Aircraft[] = {"fza_ah64d_b2e","TF373_RAF_HC5","vtx_MH60M","UK3CB_BAF_Wildcat_AH1_TRN_8A_Tropical"};
-        Amphibious[] = { "", "", "" };
-        Boats[] = {{"rhsgref_hidf_assault_boat",8,1,0.2},{"UK3CB_BAF_RHIB_HMG_MTP_RM",9,1.2,2},{"UK3CB_BAF_RHIB_GPMG_MTP_RM",9,1.2,2}};
-        hiddenSelections[] = {"screen_01","screen_02","lha_number","flag_01","flag_02"};
-        hiddenSelectionsTextures[] = {"Peral_LHA\data\clear_empty_ca.paa","Peral_LHA\data\clear_empty_ca.paa","Peral_LHA\data\clear_empty_ca.paa","\A3\Data_F\Flags\flag_uk_co.paa","Peral_LHA\data\clear_empty_ca.paa"};
     };
 };
 
